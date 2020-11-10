15992fe67c734b8faff5c21f90dcbdbf5668e7dd
Anette Kniberg <anettekniberg@gmail.com>
2020-11-10 16:32:04 +0100

    Started on datareader (Work in progress-WIP)

--- IO/ConsoleDataReader.cs
+++ IO/ConsoleDataReader.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Data;
+
 namespace StorageMaster.IO
 {
+    // Class that reads the commands, handles exceptions and passes the command to the StorageMaster
+    //Will implement interface IReader
     public class ConsoleDataReader
     {
         public ConsoleDataReader()
         {
+            var inputArray = Console.ReadLine().Split();
+            var usercommand = inputArray[0];
+            //var parameters = inputArray[
+
+
+            // 1.get the command name and parameters from user
+            // 2. validate the input
+            // 3. based on userinput (use switch statements..), call the right method from Storagemaster
+            
+
         }
     }
 }
