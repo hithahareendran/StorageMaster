e7494f688ad89e4a8ef134d9173e5a9195896794
Anette Kniberg <anettekniberg@gmail.com>
2020-11-12 13:23:54 +0100

    Done and tested. Manual merge of switches.

--- StartUp.cs
+++ StartUp.cs
@@ -11,61 +11,17 @@ namespace StorageMaster
         {
             AddProduct,
             RegisterStorage,
-            SelectVehicle
-        }
-
-        static void tempSwitch()
-        {
-
-            var storageMaster = new StorageMaster();
-
-            // 1.get the command name and parameters from user
-            var inputArray = Console.ReadLine().Split();
-            var userCommand = inputArray[0];
-            var userParameters = inputArray[1..]; //all items exept the first
-
-            Console.WriteLine(userParameters[0]);
-            Console.WriteLine(userParameters[1]);
-
-            // 2. validate the input: checking if the userinput matches available methods
-            // 3. based on userinput call the right method from Storagemaster
-            // Status! storagemasterobject.(methodname) is not firing in the switch.
-            // ---works if methods are static in storagemasterclass and just call directly.
-            if (Enum.TryParse<Commands>(userCommand, out var cmd))
-            {
-                switch (cmd)
-                {
-                    case Commands.AddProduct:
-                        storageMaster.AddProduct(userParameters[0], double.Parse(userParameters[1]));
-                        break;
-
-                    case Commands.RegisterStorage:
-                        storageMaster.RegisterStorage(userParameters[0], userParameters[1]);
-                        break;
-
-                    case Commands.SelectVehicle:
-
-                        storageMaster.SelectVehicle(userParameters[0], int.Parse(userParameters[1]));
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Unknown command");
-
-            }
+            SelectVehicle,
+            LoadVehicle,
+            SendVehicleTo,
+            UnloadVehicle,
+            GetStorageStatus,
+            END
         }
 
         static void Main(string[] args)
         {
-            tempSwitch();
-
             var storageMaster = new StorageMaster();
-
-           
 
             //read commands
             List<String> commands = new List<String>();
@@ -76,26 +32,56 @@ namespace StorageMaster
                 commands.Add(input);
             } while (input != "END");
 
-
-            //execute commands
+            //----------NEW//execute commands
             foreach (var commandWithParams in commands)
             {
                 List<String> command = commandWithParams.Split(" ").ToList();
+
+
+                // catching errors the methodcalls
                 try
                 {
-                    switch (command[0])
+                    if (Enum.TryParse<Commands>(command[0], out var cmd))
                     {
-                        case "AddProduct": Console.WriteLine(storageMaster.AddProduct(command[1], double.Parse(command[2]))); break;
-                        case "RegisterStorage": Console.WriteLine(storageMaster.RegisterStorage(command[1], command[2])); break;
-                        case "SelectVehicle": Console.WriteLine(storageMaster.SelectVehicle(command[1], int.Parse(command[2]))); break;
-                        case "LoadVehicle": Console.WriteLine(storageMaster.LoadVehicle(command.Where(item => item != command[0]))); break;
-                        case "SendVehicleTo": Console.WriteLine(storageMaster.SendVehicleTo(command[1], int.Parse(command[2]), command[3])); break;
-                        case "UnloadVehicle": Console.WriteLine(storageMaster.UnloadVehicle(command[1], int.Parse(command[2]))); break;
-                        case "GetStorageStatus": Console.WriteLine(storageMaster.GetStorageStatus(command[1])); break;
-                        case "END": Console.WriteLine(storageMaster.GetSummary()); break;
+                        switch (cmd)
+                        {
+                            case Commands.AddProduct:
+                                Console.WriteLine(storageMaster.AddProduct(command[1], double.Parse(command[2])));
+                                break;
 
-                        default: throw new InvalidOperationException("No Command " + command[0]);
+                            case Commands.RegisterStorage:
+                                Console.WriteLine(storageMaster.RegisterStorage(command[1], command[2]));
+                                break;
+
+                            case Commands.SelectVehicle:
+                                Console.WriteLine(storageMaster.SelectVehicle(command[1], int.Parse(command[2])));
+                                break;
+
+                            case Commands.LoadVehicle:
+                                Console.WriteLine(storageMaster.LoadVehicle(command.Where(item => item != command[0])));
+                                break;
+
+                            case Commands.SendVehicleTo:
+                                Console.WriteLine(storageMaster.SendVehicleTo(command[1], int.Parse(command[2]), command[3]));
+                                break;
+
+                            case Commands.UnloadVehicle:
+                                Console.WriteLine(storageMaster.UnloadVehicle(command[1], int.Parse(command[2])));
+                                break;
+
+                            case Commands.GetStorageStatus:
+                                Console.WriteLine(storageMaster.GetStorageStatus(command[1]));
+                                break;
+
+                            case Commands.END:
+                                Console.WriteLine(storageMaster.GetSummary());
+                                break;
+
+                            default:
+                                break;
+                        }
                     }
+                    else Console.WriteLine("Unknown command");
                 }
                 catch (Exception e)
                 {
@@ -103,13 +89,6 @@ namespace StorageMaster
                 }
 
             }
-
-            // // --------below creates error. Planned fix:
-            // // we need to register a storage first before. = TODO
-            //--------------------------------
-            // Console.WriteLine( "testiiing" );
-            // var controller = new WarehouseManagement.Core.StorageMaster();
-            // controller.GetStorageStatus("Awesome storage");
 
         }
     }
